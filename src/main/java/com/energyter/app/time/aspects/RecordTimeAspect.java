e201362b24da641f87993ceb50c3ba62fde2bee5
Germán Andrés Ospina Quintero <gospina@bancodebogota.com.co>
2025-04-14 00:33:08 -0500

    Add new logs and update aspectj

--- src/main/java/com/energyter/app/time/aspects/RecordTimeAspect.java
+++ src/main/java/com/energyter/app/time/aspects/RecordTimeAspect.java
@@ -1,6 +1,7 @@
 package com.energyter.app.time.aspects;
 
 import com.energyter.app.time.MeasureTime;
+import com.energyter.app.time.annotations.RecordTimeAnnotation;
 import com.energyter.app.time.impl.MeasureTimeImpl;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -13,17 +14,19 @@ public class RecordTimeAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RecordTimeAspect.class);
 
-    @Around("@annotation(com.energyter.app.time.annotations.RecordTimeAnnotation)")
+    @Around("@annotation(com.energyter.app.time.annotations)")
     public Object measureExecutionTime(ProceedingJoinPoint jointPoint) throws Throwable {
         MeasureTime measureTime = new MeasureTimeImpl();
         String methodName = jointPoint.getSignature().getName();
 
         LOGGER.info("Start time recording for method: {}", methodName);
+        System.out.print("Start time recording for method: " + methodName);
         long startTime = measureTime.getCurrentTimeInMillis();
         try {
             return jointPoint.proceed();
         } finally {
             long executionTime = startTime - measureTime.getCurrentTimeInMillis();
+            System.out.print("Execution time for method: " + methodName + " - " + executionTime);
             LOGGER.info("Execution time for method: {} - {}", methodName, executionTime);
         }
     }
