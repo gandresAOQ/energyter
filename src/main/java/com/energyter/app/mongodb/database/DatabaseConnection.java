08666aefb8e3a65e9f8c2370cbba83555ffd380a
Germán Andrés Ospina Quintero <gospina@bancodebogota.com.co>
2025-04-17 17:50:20 -0500

    Add MongoDB report

--- src/main/java/com/energyter/app/mongodb/database/DatabaseConnection.java
+++ src/main/java/com/energyter/app/mongodb/database/DatabaseConnection.java
@@ -9,12 +9,18 @@ import org.bson.Document;
 public class DatabaseConnection {
 
     private static final String URI = "mongodb+srv://germanospina12014:admin123456789@ecosystemaccountcluster.3wsyrgk.mongodb.net/?retryWrites=true&w=majority&appName=EcosystemAccountClusterTesis";
+    private static final MongoClient mongoClient = MongoClients.create(URI);
 
     public static MongoCollection<Document> getCollection() {
-        try (MongoClient mongoClient = MongoClients.create(URI)) {
-            MongoDatabase database = mongoClient.getDatabase("ecosystem_accounts");
-            MongoCollection<Document> collection = database.getCollection("performance");
-            return collection;
+        MongoDatabase database = mongoClient.getDatabase("ecosystem_accounts");
+        MongoCollection<Document> collection = database.getCollection("performance");
+        return collection;
+    }
+
+    public static void closeMongoClient() {
+        if (mongoClient != null) {
+            mongoClient.close();
         }
     }
 }
+
